f7156fed56469eaea2d75469f37093dedabe913b
Moeakwak <moeakwak@gmail.com>
2022-04-19 12:29:55 +0800

    sync language

--- src/inject.js
+++ src/inject.js
@@ -10,8 +10,20 @@ import zh_CN from "monaco-editor-nls/locale/zh-hans";
 setLocaleData(zh_CN);
 const monaco = require("monaco-editor");
 
+// load ace editor initial configs
 let ace_editor_div = $(".ace_editor");
-let ace_editor = ace.edit(ace_editor_div.attr("id"));
+let ace_editor = ace.edit(ace_editor_div.get()[0]);
+let ace_editor_session = ace_editor.getSession();
+let readOnly = ace_editor.getReadOnly();
+
+let init_language = get_ace_language(ace_editor);
+console.log("init_language", init_language);
+
+function get_ace_language() {
+  let lang = ace_editor_session.getMode().$id.replace("ace/mode/", "");
+  if ((lang = "c_cpp")) lang = "cpp";
+  return lang;
+}
 
 // change ace editor code when monaco editor change
 document.addEventListener("monaco-it-monaco-change", function (e) {
@@ -20,6 +32,7 @@ document.addEventListener("monaco-it-monaco-change", function (e) {
 });
 
 // initialize
+console.log("initialize", ace_editor.getValue());
 initialize(ace_editor.getValue());
 
 function initialize(init_code) {
@@ -52,17 +65,17 @@ function initialize(init_code) {
   ace_editor_div.after(
     `
       <div id="monaco-it-editor"
-          style="position: relative; min-height: 100px; height: 100%; width: 100%; border: 1px solid #c2c7d0">
+          style="position: relative; height: 100%; width: 100%; border: 1px solid #c2c7d0">
       </div>
     `
   );
   let monaco_div = $("#monaco-it-editor");
-  let monaco_language = "c";
-  let monaco_model = monaco.editor.createModel(init_code, monaco_language);
+  let monaco_model = monaco.editor.createModel(init_code, init_language);
   let monaco_editor = monaco.editor.create(
     document.getElementById("monaco-it-editor"),
     {
       model: monaco_model,
+      readOnly,
       automaticLayout: true,
       fontSize: 16,
       minimap: false,
@@ -102,6 +115,13 @@ function initialize(init_code) {
     // console.log("[monaco-it] cs: receive ace code change", monaco_model);
   });
 
+  // language change
+  ace_editor_session.on("changeMode", () => {
+    console.log("change language to", get_ace_language());
+    monaco.editor.setModelLanguage(monaco_model, get_ace_language());
+  });
+
+  // handle with content height change
   let ignoreEvent = false;
   const updateHeight = () => {
     let contentHeight = Math.min(1000, monaco_editor.getContentHeight());
@@ -110,7 +130,8 @@ function initialize(init_code) {
     monaco_div.height(contentHeight);
     try {
       ignoreEvent = true;
-      monaco_editor.layout({ width: 1000, height: contentHeight });
+      // monaco_editor.layout({ height: contentHeight });
+      monaco_editor.layout();
     } finally {
       ignoreEvent = false;
     }
