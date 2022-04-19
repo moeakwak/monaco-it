62eaa531ed161f55087c7b91ffd486d78c365a4e
Moeakwak <moeakwak@gmail.com>
2022-04-19 11:57:58 +0800

    fix scroll and size problem

--- src/inject.js
+++ src/inject.js
@@ -1,32 +1,25 @@
 "use strict";
 
+// use chrome-extension path for webpack chunks
 __webpack_public_path__ = document.head.dataset.monacoItPublicPath;
 
 import $ from "jquery";
 // import * as monaco from "monaco-editor";
 import { setLocaleData } from "monaco-editor-nls";
 import zh_CN from "monaco-editor-nls/locale/zh-hans";
-
 setLocaleData(zh_CN);
 const monaco = require("monaco-editor");
 
 let ace_editor_div = $(".ace_editor");
 let ace_editor = ace.edit(ace_editor_div.attr("id"));
 
-document.getMonacoItPublicPath = (p) => {
-  return (
-    $("head").attr("data-monaco-editor-public-path") +
-    require("path").basename(p)
-  );
-};
-
-// change code on receive
+// change ace editor code when monaco editor change
 document.addEventListener("monaco-it-monaco-change", function (e) {
   var code = e.detail;
   ace_editor.setValue(code);
-  // console.log("[monaco-it] worker: receive ace code change", editor);
 });
 
+// initialize
 initialize(ace_editor.getValue());
 
 function initialize(init_code) {
@@ -36,7 +29,6 @@ function initialize(init_code) {
     return new Worker(URL.createObjectURL(new Blob([iss])));
   }
   let baseUrl = $("head").attr("data-monaco-editor-public-path");
-  console.log(baseUrl);
   self.MonacoEnvironment = {
     baseUrl,
     getWorker: function (moduleId, label) {
@@ -57,11 +49,14 @@ function initialize(init_code) {
   };
 
   // create monaco editor
-  let monaco_div = ace_editor_div.after(
+  ace_editor_div.after(
     `
-      <div id="monaco-it-editor" style="position: relative; min-height: 400px; height: 100%; width: 100%; "></div>
+      <div id="monaco-it-editor"
+          style="position: relative; min-height: 100px; height: 100%; width: 100%; border: 1px solid #c2c7d0">
+      </div>
     `
   );
+  let monaco_div = $("#monaco-it-editor");
   let monaco_language = "c";
   let monaco_model = monaco.editor.createModel(init_code, monaco_language);
   let monaco_editor = monaco.editor.create(
@@ -69,7 +64,19 @@ function initialize(init_code) {
     {
       model: monaco_model,
       automaticLayout: true,
-      fontSize: 14,
+      fontSize: 16,
+      minimap: false,
+      theme: "vs", // or vs-dark
+      scrollBeyondLastLine: false,
+      wordWrap: "on",
+      wrappingStrategy: "advanced",
+      minimap: {
+        enabled: false,
+      },
+      scrollbar: {
+        alwaysConsumeMouseWheel: false,
+      },
+      overviewRulerLanes: 0,
     }
   );
   monaco_editor.layout({});
@@ -94,4 +101,20 @@ function initialize(init_code) {
     monaco_model.setValue(code);
     // console.log("[monaco-it] cs: receive ace code change", monaco_model);
   });
+
+  let ignoreEvent = false;
+  const updateHeight = () => {
+    let contentHeight = Math.min(1000, monaco_editor.getContentHeight());
+    if (contentHeight < 400) contentHeight = 400;
+    // container.style.height = `${contentHeight}px`;
+    monaco_div.height(contentHeight);
+    try {
+      ignoreEvent = true;
+      monaco_editor.layout({ width: 1000, height: contentHeight });
+    } finally {
+      ignoreEvent = false;
+    }
+  };
+  monaco_editor.onDidContentSizeChange(updateHeight);
+  updateHeight();
 }
